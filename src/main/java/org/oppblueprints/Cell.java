8785d6f6d663f9a799ae4fc14504dfd4c06b2bfc
Kyle <kylesalisbury2@gmail.com>
2022-11-16 17:00:17 +0000

    Added method definitions to Cell

--- src/main/java/org/oppblueprints/Cell.java
+++ src/main/java/org/oppblueprints/Cell.java
@@ -1,33 +1,63 @@
 package org.oppblueprints;
 
+/**
+ * The Cell class represents a single tile on a board of minesweeper. It can contain a mine, and can be revealed and subsequently clears.
+ */
 public class Cell {
 
     private CellState visualCellState = CellState.Unmined;
     private final boolean isMine;
     private boolean isCleared = false;
 
+    /**
+     * Default constructor of a cell that doesn't have a mine.
+     */
     public Cell() {
         isMine = false;
     }
 
+    /**
+     * Constructor of cell where the mine state needs to be determined.
+     * @param hasMine The mine state of the cell.
+     */
     public Cell(boolean hasMine) {
         isMine = hasMine;
     }
 
+    /**
+     * Returns if the cell contains a mine.
+     * @return True if the cell contains a mine.
+     */
     public boolean hasMine() {
         return isMine;
     }
 
+    /**
+     * Returns whether the cell has already been cleared.
+     * @return True is the cell is cleared.
+     */
     public boolean isCleared() {return isCleared;}
 
+    /**
+     * Returns the visual cell state.
+     * @return The visual cell state.
+     */
     public CellState getState() {
         return visualCellState;
     }
 
+    /**
+     * Sets the state of the Cell.
+     * @param state The new state of the cell.
+     */
     public void setState(CellState state) {
         this.visualCellState = state;
     }
 
+    /**
+     * Sets the state of the cell to how many mines surround it.
+     * @param mines The number of mines surround the cell.
+     */
     public void setMineNumber(int mines) {
         this.setState(switch (mines) {
             case 0 -> CellState.MinedNone;
@@ -44,6 +74,10 @@ public class Cell {
         });
     }
 
+    /**
+     * Reveals if the cell has a bomb or not and sets it to cleared.
+     * @return A GameResult object containing if the cell contained a mine and the game is lost.
+     */
     public GameResult reveal() {
         isCleared = true;
         if (isMine) {
@@ -53,6 +87,10 @@ public class Cell {
         return new GameResult(false);
     }
 
+    /**
+     * Sets an unflagged cell to flagged and a flagged cell to unflagged.
+     * @return A GameResult object containing if the cell flag was added or removed.
+     */
     public GameResult flag() {
         GameResult result = new GameResult(false);
         if (this.visualCellState == CellState.Flag) {
@@ -66,13 +104,17 @@ public class Cell {
         return result;
     }
 
+    /**
+     * Checks if the cell currently has a flag on it.
+     * @return True if the cell has a flag.
+     */
     public boolean isFlagged() {
         return this.visualCellState == CellState.Flag;
     }
 
     /**
-     * Checks if a cell is in the correct state for a win
-     * @return True if the cell contains a mine or
+     * Checks if a cell is in the correct state for a win.
+     * @return True if the cell contains a mine or.
      */
     public boolean isFlaggedCorrectly() {
         return (hasMine()) || (isCleared && !hasMine());
